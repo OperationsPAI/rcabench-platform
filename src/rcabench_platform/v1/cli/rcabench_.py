df8c71d73aa420b776e8beb7648c58e60da58d4a
Nugine <nugine@foxmail.com>
2025-05-18 14:40:07 +0800

    feat(cli/rcabench): kube info

--- src/rcabench_platform/v1/cli/rcabench_.py
+++ src/rcabench_platform/v1/cli/rcabench_.py
@@ -1,7 +1,11 @@
+from ..utils.serde import save_json
+from ..clients.k8s import download_kube_info
+from ..folders import TEMP
 from ..clients.rcabench_ import CustomRCABenchSDK
 from ..logging import logger, timeit
 
 from pprint import pprint
+from pathlib import Path
 
 import typer
 
@@ -15,3 +19,10 @@ def query_dataset(name: str):
 
     output = sdk.query_dataset(name=name)
     pprint(output)
+
+
+@app.command()
+@timeit()
+def kube_info(save_path: Path = TEMP / "kube_info.json"):
+    kube_info = download_kube_info(ns="ts1")
+    save_json(kube_info.to_dict(), path=save_path)
