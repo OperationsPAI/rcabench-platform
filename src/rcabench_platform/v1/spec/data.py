b6470540e0937199c9f706ab92f5665ebc94d2fa
Nugine <nugine@foxmail.com>
2025-05-22 18:12:19 +0800

    feat(spec/data): find_ground_truths

--- src/rcabench_platform/v1/spec/data.py
+++ src/rcabench_platform/v1/spec/data.py
@@ -9,6 +9,9 @@ TEMP_SDG = TEMP / "sdg"
 DATA_ROOT = Path("data") / "rcabench_platform_datasets"
 META_ROOT = DATA_ROOT / "__meta__"
 
+OUTPUT = Path("output")
+OUTPUT_META = OUTPUT / "__meta__"
+
 
 def dataset_index_path(dataset: str) -> Path:
     return META_ROOT / dataset / "index.parquet"
@@ -34,6 +37,31 @@ def get_datapack_list(dataset: str) -> list[tuple[str, Path]]:
         ans.append((datapack, datapack_folder))
 
     return ans
+
+
+def find_ground_truths(dataset: str, datapack: str) -> set[tuple[str, str]]:
+    label_path = dataset_label_path(dataset)
+
+    label_df = (
+        pl.scan_parquet(label_path)
+        .filter(
+            pl.col("dataset") == dataset,
+            pl.col("datapack") == datapack,
+        )
+        .collect()
+    )
+
+    assert len(label_df) >= 1, f"label of datapack `{datapack}` is not found in dataset `{dataset}`"
+
+    ground_truths = set()
+    for level, name in label_df[["gt.level", "gt.name"]].iter_rows():
+        assert isinstance(level, str)
+        assert isinstance(name, str)
+        ground_truths.add((level, name))
+
+    assert len(ground_truths) == len(label_df)
+
+    return ground_truths
 
 
 if __name__ == "__main__":
