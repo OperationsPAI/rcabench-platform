8e32c86016c69b53218ff48d54c5d97c18a257d8
Nugine <nugine@foxmail.com>
2025-06-03 15:50:44 +0800

    feat(datasets/convert_rcabench): extract from cli

--- cli/make_rcabench.py
+++ cli/make_rcabench.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env -S uv run -s
 from rcabench_platform.v1.cli.main import app, logger
 from rcabench_platform.v1.clients.rcabench_ import get_mariadb_connection
-from rcabench_platform.v1.datasets.rcabench_ import rcabench_get_service_name, FAULT_TYPES
+from rcabench_platform.v1.datasets.rcabench_ import FAULT_TYPES
+from rcabench_platform.v1.datasets.convert_rcabench import RcabenchDatapackLoader, RcabenchDatasetLoader
 from rcabench_platform.v1.logging import timeit
-from rcabench_platform.v1.spec.convert import (
-    DatasetLoader,
-    DatapackLoader,
-    Label,
-    convert_datapack,
-    convert_dataset,
-    postprocess_collect_schema,
-)
+from rcabench_platform.v1.spec.convert import convert_datapack, convert_dataset
 from rcabench_platform.v1.spec.data import DATA_ROOT, META_ROOT, TEMP, get_datapack_list
 from rcabench_platform.v1.utils.dataframe import print_dataframe
 from rcabench_platform.v1.utils.dict_ import flatten_dict
@@ -29,393 +23,9 @@ from tqdm.auto import tqdm
 import dateutil.tz
 
 
-def local_to_utc(col: pl.Expr) -> pl.Expr:
-    col = col.dt.replace_time_zone("Asia/Shanghai")
-    return col.dt.cast_time_unit("ns").dt.convert_time_zone("UTC")
-
-
-def replace_time_col(lf: pl.LazyFrame, col_name: str) -> pl.LazyFrame:
-    lf = lf.with_columns(local_to_utc(pl.col(col_name))).rename({col_name: "time"})
-    return lf
-
-
-def unnest_json_col(lf: pl.LazyFrame, col_name: str, dtype: pl.Struct) -> pl.LazyFrame:
-    lf = lf.with_columns(pl.col(col_name).str.json_decode(dtype).struct.unnest()).drop(col_name)
-    return lf
-
-
-def convert_metrics(src: Path) -> pl.LazyFrame:
-    lf = pl.scan_parquet(src).select(
-        "TimeUnix",
-        "MetricName",
-        "Value",
-        "ResourceAttributes",
-    )
-
-    lf = replace_time_col(lf, "TimeUnix")
-
-    lf = lf.rename({"MetricName": "metric", "Value": "value"})
-
-    lf = lf.with_columns(pl.lit(None, dtype=pl.String).alias("service_name"))
-
-    resource_attributes = pl.Struct(
-        [
-            pl.Field("k8s.node.name", pl.String),
-            pl.Field("k8s.namespace.name", pl.String),
-            pl.Field("k8s.statefulset.name", pl.String),
-            pl.Field("k8s.deployment.name", pl.String),
-            pl.Field("k8s.replicaset.name", pl.String),
-            pl.Field("k8s.pod.name", pl.String),
-            pl.Field("k8s.container.name", pl.String),
-        ]
-    )
-    lf = unnest_json_col(lf, "ResourceAttributes", resource_attributes)
-
-    attr_cols = [field.name for field in resource_attributes.fields]
-    lf = lf.rename({col: "attr." + col for col in attr_cols})
-
-    lf = lf.sort("time")
-
-    return lf
-
-
-def convert_metrics_histogram(src: Path) -> pl.LazyFrame:
-    lf = pl.scan_parquet(src).select(
-        "TimeUnix",
-        "MetricName",
-        "ServiceName",
-        "ResourceAttributes",
-        "Attributes",
-        "Count",
-        "Sum",
-        # "BucketCounts",
-        # "ExplicitBounds",
-        "Min",
-        "Max",
-        # "AggregationTemporality",
-    )
-
-    lf = replace_time_col(lf, "TimeUnix")
-
-    lf = lf.rename(
-        {
-            "MetricName": "metric",
-            "ServiceName": "service_name",
-            "Count": "count",
-            "Sum": "sum",
-            "Min": "min",
-            "Max": "max",
-        }
-    )
-
-    lf = lf.with_columns(
-        pl.col("count").cast(pl.Float64),
-        pl.col("sum").cast(pl.Float64),
-        pl.col("min").cast(pl.Float64),
-        pl.col("max").cast(pl.Float64),
-    )
-
-    resource_attributes = pl.Struct(
-        [
-            pl.Field("pod.name", pl.String),
-            pl.Field("service.name", pl.String),
-            pl.Field("service.namespace", pl.String),
-        ]
-    )
-    lf = unnest_json_col(lf, "ResourceAttributes", resource_attributes)
-    lf = lf.rename(
-        {
-            "pod.name": "attr.k8s.pod.name",
-            "service.name": "attr.k8s.service.name",
-            "service.namespace": "attr.k8s.namespace.name",
-        }
-    )
-
-    attributes = pl.Struct(
-        [
-            pl.Field("jvm.gc.action", pl.String),
-            pl.Field("jvm.gc.name", pl.String),
-        ]
-    )
-    lf = unnest_json_col(lf, "Attributes", attributes)
-    lf = lf.rename(
-        {
-            "jvm.gc.action": "attr.jvm.gc.action",
-            "jvm.gc.name": "attr.jvm.gc.name",
-        }
-    )
-
-    lf = lf.sort("time")
-    return lf
-
-
-def convert_traces(src: Path) -> pl.LazyFrame:
-    lf = pl.scan_parquet(src).select(
-        "Timestamp",
-        "TraceId",
-        "SpanId",
-        "ParentSpanId",
-        "SpanName",
-        "SpanKind",
-        "ServiceName",
-        "ResourceAttributes",
-        "SpanAttributes",
-        "Duration",
-        "StatusCode",
-    )
-
-    lf = replace_time_col(lf, "Timestamp")
-
-    lf = lf.rename(
-        {
-            "TraceId": "trace_id",
-            "SpanId": "span_id",
-            "ParentSpanId": "parent_span_id",
-            "SpanName": "span_name",
-            "ServiceName": "service_name",
-            "Duration": "duration",
-            "SpanKind": "attr.span_kind",
-            "StatusCode": "attr.status_code",
-        }
-    )
-
-    resource_attributes = pl.Struct(
-        [
-            pl.Field("pod.name", pl.String),
-            pl.Field("service.name", pl.String),
-            pl.Field("service.namespace", pl.String),
-        ]
-    )
-    lf = unnest_json_col(lf, "ResourceAttributes", resource_attributes)
-    lf = lf.rename(
-        {
-            "pod.name": "attr.k8s.pod.name",
-            "service.name": "attr.k8s.service.name",
-            "service.namespace": "attr.k8s.namespace.name",
-        }
-    )
-
-    span_attributes = pl.Struct(
-        [
-            # "telemetry.sdk.language": "go"
-            pl.Field("http.method", pl.String),
-            pl.Field("http.request_content_length", pl.String),
-            pl.Field("http.response_content_length", pl.String),
-            pl.Field("http.status_code", pl.String),
-            #
-            # "telemetry.sdk.language": "java"
-            pl.Field("http.request.method", pl.String),
-            pl.Field("http.response.status_code", pl.String),
-            #
-        ]
-    )
-    lf = unnest_json_col(lf, "SpanAttributes", span_attributes)
-
-    coalesce_columns = [
-        ("http.request.method", "http.method"),
-        ("http.response.status_code", "http.status_code"),
-    ]
-    lf = lf.with_columns([pl.coalesce(*cols).alias(cols[0]) for cols in coalesce_columns])
-    lf = lf.drop([cols[1] for cols in coalesce_columns])
-
-    lf = lf.with_columns(
-        pl.col("http.request_content_length").cast(pl.UInt64),
-        pl.col("http.response_content_length").cast(pl.UInt64),
-        pl.col("http.response.status_code").cast(pl.UInt16),
-    )
-
-    lf = lf.rename(
-        {
-            "http.request.method": "attr.http.request.method",
-            "http.response.status_code": "attr.http.response.status_code",
-            "http.request_content_length": "attr.http.request.content_length",
-            "http.response_content_length": "attr.http.response.content_length",
-        }
-    )
-
-    lf = lf.sort("time")
-
-    return lf
-
-
-def convert_logs(src: Path) -> pl.LazyFrame:
-    lf = pl.scan_parquet(src).select(
-        "Timestamp",
-        "TraceId",
-        "SpanId",
-        "SeverityText",
-        "ServiceName",
-        "Body",
-        "ResourceAttributes",
-        # "LogAttributes",
-    )
-
-    lf = replace_time_col(lf, "Timestamp")
-
-    lf = lf.rename(
-        {
-            "TraceId": "trace_id",
-            "SpanId": "span_id",
-            "ServiceName": "service_name",
-            "SeverityText": "level",
-            "Body": "message",
-        }
-    )
-
-    lf = lf.with_columns(
-        pl.col("level").str.to_uppercase(),
-    )
-
-    resource_attributes = pl.Struct(
-        [
-            pl.Field("pod.name", pl.String),
-            pl.Field("service.name", pl.String),
-            pl.Field("service.namespace", pl.String),
-        ]
-    )
-    lf = unnest_json_col(lf, "ResourceAttributes", resource_attributes)
-    lf = lf.rename(
-        {
-            "pod.name": "attr.k8s.pod.name",
-            "service.name": "attr.k8s.service.name",
-            "service.namespace": "attr.k8s.namespace.name",
-        }
-    )
-
-    lf = lf.sort("time")
-
-    return lf
-
-
-def convert_conclusion(src: Path) -> pl.LazyFrame:
-    lf = pl.scan_csv(
-        src,
-        schema={
-            "SpanName": pl.String,
-            "Issues": pl.String,
-            "AbnormalAvgDuration": pl.Float64,
-            "NormalAvgDuration": pl.Float64,
-            "AbnormalSuccRate": pl.Float64,
-            "NormalSuccRate": pl.Float64,
-            "AbnormalP90": pl.Float64,
-            "NormalP90": pl.Float64,
-            "AbnormalP95": pl.Float64,
-            "NormalP95": pl.Float64,
-            "AbnormalP99": pl.Float64,
-            "NormalP99": pl.Float64,
-        },
-    )
-    return lf
-
-
-class RcabenchDatapackLoader(DatapackLoader):
-    def __init__(self, src_folder: Path, datapack: str) -> None:
-        self._src_folder = src_folder
-        self._datapack = datapack
-
-        self._service = rcabench_get_service_name(datapack)
-
-        injection = load_json(path=self._src_folder / "injection.json")
-        self._fault_type: str = FAULT_TYPES[injection["fault_type"]]
-        self._injection_config = json.loads(injection["display_config"])
-
-    def name(self) -> str:
-        return self._datapack
-
-    def labels(self) -> list[Label]:
-        if self._fault_type.startswith("Network"):
-            injection_point = self._injection_config.get("injection_point")
-            assert isinstance(injection_point, dict)
-
-            source_service = injection_point.get("source_service")
-            target_service = injection_point.get("target_service")
-
-            if source_service and target_service:
-                return [
-                    Label(level="service", name=source_service),
-                    Label(level="service", name=target_service),
-                ]
-
-        return [Label(level="service", name=self._service)]
-
-    def data(self) -> dict[str, Any]:
-        ans: dict[str, Any] = {
-            "env.json": self._src_folder / "env.json",
-            "injection.json": self._src_folder / "injection.json",
-            "conclusion.parquet": convert_conclusion(self._src_folder / "conclusion.csv"),
-        }
-
-        converters = {
-            "_traces": convert_traces,
-            "_logs": convert_logs,
-            "_metrics": convert_metrics,
-            "_metrics_histogram": convert_metrics_histogram,
-        }
-
-        for key, func in converters.items():
-            for prefix in ("normal", "abnormal"):
-                name = f"{prefix}{key}.parquet"
-                ans[name] = func(self._src_folder / name)
-
-        return ans
-
-    def postprocess(self, folder: Path, files: set[str]) -> None:
-        schema = postprocess_collect_schema(folder, files)
-        save_json(schema, path=folder / "schema.json")
-
-
-@timeit()
-def scan_datapacks(src_root: Path) -> list[str]:
-    datapacks = []
-    for path in src_root.iterdir():
-        if not path.is_dir():
-            continue
-
-        if not (path / "injection.json").exists():
-            continue
-
-        total_size = 0
-        for file in path.iterdir():
-            assert file.is_file()
-            total_size += file.stat().st_size
-        total_size_mib = total_size / (1024 * 1024)
-
-        if total_size_mib > 500:
-            logger.warning(f"Skipping large datapack `{path.name}` with size {total_size_mib:.2f} MiB")
-            continue
-
-        mtime = path.stat().st_mtime
-        datapacks.append((path.name, mtime))
-
-    datapacks.sort(key=lambda x: x[1])
-
-    return [name for name, _ in datapacks]
-
-
-class RcabenchDatasetLoader(DatasetLoader):
-    def __init__(self, src_root: Path, dataset: str) -> None:
-        self._src_root = src_root
-        self._dataset = dataset
-
-        self._datapacks = scan_datapacks(src_root)
-
-    def name(self) -> str:
-        return self._dataset
-
-    def __len__(self) -> int:
-        return len(self._datapacks)
-
-    def __getitem__(self, index: int) -> DatapackLoader:
-        datapack = self._datapacks[index]
-        return RcabenchDatapackLoader(
-            src_folder=self._src_root / datapack,
-            datapack=datapack,
-        )
-
-
 @app.command()
 @timeit()
-def run(skip: bool = True, parallel: int | None = 4):
+def run(skip: bool = True, parallel: int | None = 4, scan: bool = True):
     src_root = Path("data") / "rcabench_dataset"
 
     loader = RcabenchDatasetLoader(src_root, dataset="rcabench")
@@ -427,7 +37,8 @@ def run(skip: bool = True, parallel: int | None = 4):
         ignore_exceptions=True,
     )
 
-    scan_datapack_attributes()
+    if scan:
+        scan_datapack_attributes()
 
 
 @app.command()
@@ -446,6 +57,7 @@ def local_test_1():
 
 
 @app.command()
+@timeit()
 def scan_datapack_attributes():
     dataset = "rcabench"
     datapacks = get_datapack_list(dataset)
@@ -887,24 +499,24 @@ def query_fault_types(dataset: str):
     print_dataframe(fault_types_count)
 
 
-@app.command()
-def patch_conclusion_files():
-    src = Path("data") / "rcabench_dataset"
+# @app.command()
+# def patch_conclusion_files():
+#     src = Path("data") / "rcabench_dataset"
 
-    datapacks = get_datapack_list("rcabench")
+#     datapacks = get_datapack_list("rcabench")
 
-    for datapack_name, datapack_path in tqdm(datapacks):
-        if not datapack_path.is_dir():
-            continue
+#     for datapack_name, datapack_path in tqdm(datapacks):
+#         if not datapack_path.is_dir():
+#             continue
 
-        src_path = src / datapack_name / "conclusion.csv"
-        dst_path = datapack_path / "conclusion.parquet"
+#         src_path = src / datapack_name / "conclusion.csv"
+#         dst_path = datapack_path / "conclusion.parquet"
 
-        if not src_path.exists():
-            continue
+#         if not src_path.exists():
+#             continue
 
-        df = convert_conclusion(src_path).collect()
-        save_parquet(df, path=dst_path)
+#         df = convert_conclusion(src_path).collect()
+#         save_parquet(df, path=dst_path)
 
 
 if __name__ == "__main__":
