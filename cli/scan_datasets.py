0fe81db7aa00bbe925ba727b1d77ae1e05a09ef0
Nugine <nugine@foxmail.com>
2025-06-14 13:10:47 +0800

    feat(cli/scan_datasets): plot_rows_count

--- cli/scan_datasets.py
+++ cli/scan_datasets.py
@@ -2,13 +2,14 @@
 from rcabench_platform.v2.cli.main import app, logger, timeit
 from rcabench_platform.v2.datasets.spec import get_datapack_folder, get_datapack_list, get_dataset_meta_file
 from rcabench_platform.v2.utils.fmap import fmap_threadpool
+from rcabench_platform.v2.utils.serde import save_parquet
 
 from typing import Any
 import functools
 
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 import polars as pl
-
-from rcabench_platform.v2.utils.serde import save_parquet
 
 
 @app.command()
@@ -62,6 +63,33 @@ def _scan_rows_count(dataset: str, datapack: str) -> dict[str, Any]:
         ans[name + ".rows"] = count
 
     return ans
+
+
+@app.command()
+@timeit()
+def plot_rows_count(show: bool = True):
+    datasets = ["rcaeval_re2_tt", "rcabench_filtered"]
+
+    df_list = []
+    for dataset in datasets:
+        df = pl.read_parquet(get_dataset_meta_file(dataset, "rows_count.parquet"))
+        df_list.append(df)
+
+    columns = ["traces.rows", "metrics.rows"]
+
+    fig, axes = plt.subplots(1, len(columns), figsize=(20, 10))
+    for i, column in enumerate(columns):
+        ax: Axes = axes[i]
+        for j, df in enumerate(df_list):
+            ax.boxplot(df[column], positions=[j], widths=0.6, vert=True)
+        ax.set_title(column)
+        ax.set_xticks(range(len(df_list)))
+        ax.set_xticklabels(datasets)
+    plt.tight_layout()
+    plt.savefig("temp/rows_count.png")
+
+    if show:
+        plt.show()
 
 
 if __name__ == "__main__":
