669e8db2db24a993181a399d437edbc5649cba5b
lincyaw <814750204@qq.com>
2025-07-01 18:28:42 +0800

    Fix root in ParentID

--- cli/make_nezha.py
+++ cli/make_nezha.py
@@ -123,6 +123,9 @@ class NezhaDatapackLoader(DatapackLoader):
 
                 df = pl.read_csv(trace_file)
 
+                ##处理ParentID=root
+                df = df.with_columns(pl.col("ParentID").str.replace("root", ""))
+
                 column_mapping = {
                     "trace_id": "TraceID",
                     "span_id": "SpanID",
