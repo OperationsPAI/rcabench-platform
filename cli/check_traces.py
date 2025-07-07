ef6ac4514142277a5532f6f3761e3aca1bf6da61
Nugine <nugine@foxmail.com>
2025-07-07 23:57:32 +0800

    feat(cli/check_traces): use subquery

--- cli/check_traces.py
+++ cli/check_traces.py
@@ -2,11 +2,10 @@
 from rcabench_platform.v2.cli.main import app, logger, timeit
 from rcabench_platform.v2.clients.clickhouse import get_clickhouse_client, query_parquet_stream
 from rcabench_platform.v2.config import get_config
+from rcabench_platform.v2.utils.dataframe import print_dataframe
 from rcabench_platform.v2.utils.fmap import fmap_threadpool
 from rcabench_platform.v2.utils.serde import save_parquet
 
-from tempfile import TemporaryDirectory
-from pathlib import Path
 import functools
 
 import polars as pl
@@ -14,7 +13,7 @@ import polars as pl
 
 @app.command()
 @timeit()
-def check_loadgenerator(limit: int = 1000, parallel: int = 16):
+def check_loadgenerator(limit: int = 10000, parallel: int = 16):
     assert limit > 0
 
     temp = get_config().temp
@@ -23,29 +22,33 @@ def check_loadgenerator(limit: int = 1000, parallel: int = 16):
 
     with get_clickhouse_client() as client:
         query = f"""
+        WITH target_traces AS (
+            SELECT      TraceId
+            FROM        otel_traces
+            WHERE       ServiceName = 'loadgenerator-service' AND ParentSpanId = ''
+            ORDER BY    Timestamp DESC
+            LIMIT       {limit}
+        )
         SELECT      *
         FROM        otel_traces
-        WHERE       ServiceName = 'loadgenerator-service' AND ParentSpanId = ''
-        ORDER BY    Timestamp DESC
-        LIMIT       {limit}
+        WHERE       TraceId IN (SELECT TraceId FROM target_traces)
+        ORDER BY    TraceId, Timestamp ASC
         """
-        save_path = temp / "loadgenerator_traces.parquet"
+        save_path = temp / "loadgenerator_traces_all.parquet"
         query_parquet_stream(client, query, save_path)
 
     df: pl.DataFrame = pl.read_parquet(save_path)
     assert len(df) > 0, "No traces found"
 
-    trace_id_list = df["TraceId"].unique().to_list()
+    loadgenerator_df = df.filter(
+        pl.col("ServiceName") == "loadgenerator-service",
+        pl.col("ParentSpanId") == "",
+    )
+    trace_id_list = loadgenerator_df["TraceId"].unique().to_list()
+
     logger.info(f"Found {len(trace_id_list)} unique traces")
 
-    with TemporaryDirectory() as tempdir:
-        tasks = [functools.partial(_query_trace, trace_id, Path(tempdir)) for trace_id in trace_id_list]
-        df_paths = fmap_threadpool(tasks, parallel=parallel)
-        df = pl.read_parquet(df_paths)
-
-    save_parquet(df, path=temp / "loadgenerator_traces_all.parquet")
-
-    tasks = [functools.partial(_check_trace, trace_id, df) for trace_id in trace_id_list]
+    tasks = [functools.partial(_check_trace, trace_id, df, loadgenerator_df) for trace_id in trace_id_list]
     results = fmap_threadpool(tasks, parallel=parallel)
     invalid_count = sum(not result for result in results)
 
@@ -54,29 +57,29 @@ def check_loadgenerator(limit: int = 1000, parallel: int = 16):
     else:
         logger.info(f"All {len(trace_id_list)} traces have spans from other services")
 
+    duration_df = df.select(pl.col("Duration").truediv(1e9).alias("duration")).select(
+        pl.col("duration").mean().alias("duration:mean"),
+        pl.col("duration").std().alias("duration:std"),
+        pl.col("duration").min().alias("duration:min"),
+        pl.col("duration").median().alias("duration:median"),
+        pl.col("duration").max().alias("duration:max"),
+        pl.col("duration").quantile(0.90).alias("duration:P90"),
+        pl.col("duration").quantile(0.95).alias("duration:P95"),
+        pl.col("duration").quantile(0.99).alias("duration:P99"),
+        pl.col("duration").quantile(0.999).alias("duration:P999"),
+        pl.col("duration").quantile(0.9999).alias("duration:P9999"),
+    )
+    print_dataframe(duration_df)
 
-@timeit(log_args={"trace_id"})
-def _query_trace(trace_id: str, save_dir: Path) -> Path:
-    with get_clickhouse_client() as client:
-        query = f"""
-        SELECT      *
-        FROM        otel_traces
-        WHERE       TraceId = '{trace_id}'
-        ORDER BY    Timestamp ASC
-        LIMIT       1000
-        """
-        temp_path = save_dir / f"{trace_id}.parquet"
-        query_parquet_stream(client, query, temp_path)
-        return temp_path
 
-
-def _check_trace(trace_id: str, df: pl.DataFrame) -> bool:
+def _check_trace(trace_id: str, df: pl.DataFrame, loadgenerator_df: pl.DataFrame) -> bool:
     trace_df = df.filter(
         pl.col("TraceId") == trace_id,
         pl.col("ServiceName") != "loadgenerator-service",
     )
     if len(trace_df) == 0:
-        logger.warning(f"Trace `{trace_id}` has no spans from other services")
+        timestamp = loadgenerator_df.filter(pl.col("TraceId") == trace_id)["Timestamp"].item()
+        logger.warning(f"Trace `{trace_id}` ({timestamp}) has no spans from other services")
         return False
     return True
 
