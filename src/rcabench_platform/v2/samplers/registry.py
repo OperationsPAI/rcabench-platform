2367035be8c5de67ad52d0f241aadb45a2aefa34
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2025-08-30 21:42:12 +0800

    release v0.3.34b3
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- src/rcabench_platform/v2/samplers/registry.py
+++ src/rcabench_platform/v2/samplers/registry.py
@@ -4,9 +4,14 @@ from .random_ import RandomSampler
 from .spec import register_sampler
 
 
+def _create_random_sampler():
+    """Factory function to create RandomSampler instance."""
+    return RandomSampler()
+
+
 def _register_default_samplers():
     """Register default sampler algorithms."""
-    register_sampler("random", lambda: RandomSampler())
+    register_sampler("random", _create_random_sampler)
 
 
 # Automatically register default samplers when module is imported
